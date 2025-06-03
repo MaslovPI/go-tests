47eeed59639069740a222e63183078359612893d
Pavel Maslov <dnfanl@gmail.com>
2025-06-03 22:16:07 +0200

    Hello, World

---
+++ 1_Hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/maslovpi/go-tests
+
+go 1.24.3
---
+++ 1_Hello/hello_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Pavel", "")
+		want := "Hello, Pavel"
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Oondine", "French")
+		want := "Bonjour, Oondine"
+		asserCorrectMessage(t, got, want)
+	})
+}
+
+func asserCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
