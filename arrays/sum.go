fbeca7fc00b3352db20023368cc1360a2c630742
Pavel Maslov <dnfanl@gmail.com>
2025-06-05 11:43:26 +0200

    Sum all tests

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,24 @@ func Sum(array []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, slice := range numbersToSum {
+		sums = append(sums, Sum(slice))
+	}
+	return sums
+}
+
+func SumAllTail(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, slice := range numbersToSum {
+		if len(slice) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := slice[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,15 +1,46 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("Should give correct sum with array of fixed size", func(t *testing.T) {
-		array := []int{1, 2, 3, 4, 5}
-		got := Sum(array)
+	t.Run("Should give correct sum with slice", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 5}
+		got := Sum(slice)
 		want := 15
 
 		if want != got {
-			t.Errorf("Want: %d, Got: %d, Given: %v", want, got, array)
+			t.Errorf("Want: %d, Got: %d, Given: %v", want, got, slice)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Should give slice of sums for provided slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{0, 9})
+		want := []int{6, 9}
+		assertSumsEqual(t, want, got)
+	})
+}
+
+func TestSumAllTail(t *testing.T) {
+	t.Run("Should give slice of tail sums for provided slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2, 3}, []int{0, 9})
+		want := []int{5, 9}
+		assertSumsEqual(t, want, got)
+	})
+	t.Run("Should safely process empty slice", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2, 3}, []int{})
+		want := []int{5, 0}
+		assertSumsEqual(t, want, got)
+	})
+}
+
+func assertSumsEqual(t testing.TB, want, got []int) {
+	t.Helper()
+	if !slices.Equal(want, got) {
+		t.Errorf("Want: %v, Got: %v", want, got)
+	}
 }
