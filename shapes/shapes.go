6f7a72c8c2399739e4da8c24adff021faf2b577a
Pavel Maslov <dnfanl@gmail.com>
2025-06-05 14:08:03 +0200

    Structs and interfaces tests

---
+++ shapes/shapes.go
@@ -0,0 +1,48 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	A float64
+	B float64
+	C float64
+}
+
+func (t Triangle) Area() float64 {
+	s := t.Perimeter() / 2
+	return math.Sqrt(s * (s - t.A) * (s - t.B) * (s - t.C))
+}
+
+func (t Triangle) Perimeter() float64 {
+	return t.A + t.B + t.C
+}
